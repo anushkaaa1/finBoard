736074e4569ef907e3725fcc2a28e5705c477904
anushkaaa1 <anushkakumari4124@gmail.com>
2026-05-29 18:01:09 +0530

    fix: adjust bundler commonjs options for production compile1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin' // or whatever your config currently uses
+import react from '@vitejs/plugin-react'// or whatever your config currently uses
 
 export default defineConfig({
   plugins: [react()],
